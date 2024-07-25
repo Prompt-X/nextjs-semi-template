661bd7e7320815d89b1554f70ede822c02545add
linpan <yidiyu0507s@163.com>
2024-07-25 15:14:46 +0800

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import { Button, Toast } from '@douyinfe/semi-ui';
+import { Button } from '@douyinfe/semi-ui';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <Button
-      className={'bg-emerald-500 rounded-3xl'}>Hello Semi</Button>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-white p-24">
+      <Button className={'rounded-3xl bg-emerald-500'}>Hello Semi</Button>
     </main>
   );
 }
