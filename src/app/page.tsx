6b8011cee2dd7ad8ca0470b1fd7b49a33ffb9968
linpan <yidiyu0507s@163.com>
2024-07-27 19:39:28 +0800

    update:config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-'use client';
+'use client'
 
-import { Button } from '@douyinfe/semi-ui';
+import { Button } from '@douyinfe/semi-ui'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-white p-24">
-      <Button className={'rounded-3xl bg-emerald-500'}>Hello Semi</Button>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between bg-white p-24">
+            <Button className={'rounded-3xl bg-emerald-500'}>Hello Semi</Button>
+        </main>
+    )
 }
